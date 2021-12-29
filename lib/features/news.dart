f2459a324a7afaab9946e761087035a6f6ede916
Anantharao1998 <anantharao1998@gmail.com>
2021-12-29 13:45:44 +0800

    Added news feature to the home screen

--- lib/features/news.dart
+++ lib/features/news.dart
@@ -1,40 +1,26 @@
+// ignore_for_file: camel_case_types
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class HomePage extends StatelessWidget {
+class newsContainer extends StatelessWidget {
   news() async {
     var url;
     url = await http.get(
         Uri.parse("https://min-api.cryptocompare.com/data/v2/news/?lang=EN"));
-    print(url.body);
     return json.decode(url.body)['Data'];
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-      
-      backgroundColor: Color(0xff191826),
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text(
-          'NEWS',
-          style: TextStyle(fontSize: 25.0, color: Color(0xfff43370)),
-        ),
-        backgroundColor: Color(0xff191826),
-      ),
-
-      body: FutureBuilder(
-        
-          future: news(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            
-            if (snapshot.hasData) {
-              return CarouselSlider.builder(
+    return FutureBuilder(
+        future: news(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return CarouselSlider.builder(
                 options: CarouselOptions(
                   autoPlay: true,
                   pauseAutoPlayOnTouch: true,
@@ -44,44 +30,46 @@ class HomePage extends StatelessWidget {
                     (BuildContext context, int index, int pageViewIndex) =>
                         InkWell(
                           onTap: () {
-                          launch(
-                              snapshot.data[index]["url"],);
-                        },
-                  child: Card(
-            
-                                  child: Padding(
-                                  padding: const EdgeInsets.fromLTRB(15, 1, 15, 0),
-
-                   
-                    child: Center(
-                      child: Column(
-
-                        
-                        
-                          children: [
-                            Image.network(snapshot.data[index]['imageurl'],  
-                                           height: 120,
-                                          fit: BoxFit.fitWidth ),
-                          SizedBox(height: 10,),
-                          Text(
-                          snapshot.data[index]["title"],
-                                                    textAlign: TextAlign.justify,
-                          style: TextStyle(color: Colors.black,fontSize: 16,),
-                        ),
-                          ],
-                        
-                        
-                      ),
-                    ),
-                  ),
-                ),
-              ));
-              
-            }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }),
-    );
+                            launch(
+                              snapshot.data[index]["url"],
+                            );
+                          },
+                          child: Card(
+                            elevation: 20,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(15, 10, 15, 2),
+                              child: Container(
+                                decoration: BoxDecoration(),
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 10.0),
+                                      child: Image.network(
+                                          snapshot.data[index]['imageurl'],
+                                          height: 120,
+                                          fit: BoxFit.fitWidth),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      snapshot.data[index]["title"],
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ));
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
